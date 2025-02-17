894f9878700ace247597d6ff14cfafb441156473
bangkatul123 <bangkatul8@gmail.com>
2025-02-17 18:03:55 +0700

    thx
    thx

---
+++ thx.js
@@ -0,0 +1,6 @@
+const heartIcon = document.querySelector('.heart');
+
+heartIcon.addEventListener('click', () => {
+    alert('Terima kasih atas cintanya, Ibu!'); // Menampilkan alert saat ikon hati diklik
+    });
+    
