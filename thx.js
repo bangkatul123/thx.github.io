086cbf4bf15473743c36a4569a27d1da5df1ce21
bangkatul123 <bangkatul8@gmail.com>
2025-02-17 18:28:02 +0700

    Delete thx.js

--- thx.js
+++
@@ -1,6 +0,0 @@
-const heartIcon = document.querySelector('.heart');
-
-heartIcon.addEventListener('click', () => {
-    alert('Terima kasih atas cintanya, Ibu!'); // Menampilkan alert saat ikon hati diklik
-    });
-    
